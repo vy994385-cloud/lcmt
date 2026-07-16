0002889ebba642f2f443f6444815529018e2bfb6
Vishal Yadav <vy994385@gmail.com>
2026-07-16 20:40:45 +0530

    Add like user API route

--- backend/src/routes/profileRoutes.ts
+++ backend/src/routes/profileRoutes.ts
@@ -4,16 +4,47 @@ import {
   updateProfile,
   getMyProfile,
   getUsers,
+  likeUser,
 } from "../controllers/profileController"
 
 import { protect } from "../middleware/authMiddleware"
 
+
 const router = express.Router()
 
-router.put("/profile", protect, updateProfile)
 
-router.get("/profile/me", protect, getMyProfile)
 
-router.get("/users", protect, getUsers)
+router.put(
+  "/profile",
+  protect,
+  updateProfile
+)
+
+
+
+router.get(
+  "/profile/me",
+  protect,
+  getMyProfile
+)
+
+
+
+router.get(
+  "/users",
+  protect,
+  getUsers
+)
+
+
+
+// Like a user
+router.post(
+  "/users/:userId/like",
+  protect,
+  likeUser
+)
+
+
 
 export default router
